6cf762d5c080503a977859378c5b7ae499974785
Mahir Asef <darkknight5812@gmail.com>
2026-03-16 03:54:44 +0600

    easy solution

---
+++ Codeforces/800-100 Rating/1194A_Remove_a_Progression.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define endl '\n'
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t, n, x;
+    cin >> t;
+    while (t--)
+    {
+        cin >> n >> x;
+        cout << 2 * x << endl;
+    }
+}
