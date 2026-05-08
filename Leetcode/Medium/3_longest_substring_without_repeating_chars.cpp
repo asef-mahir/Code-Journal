08f14639b3c1035a69fb5d3156d740413358bd68
asef-mahir <darkknight5812@gmail.com>
2026-05-08 17:08:55 +0600

    longest substring w/o repeat using sliding window

---
+++ Leetcode/Medium/3_longest_substring_without_repeating_chars.cpp
@@ -0,0 +1,37 @@
+/* Problem link: https://leetcode.com/problems/longest-substring-without-repeating-characters/ 
+Technique: Sliding Window 
+Time Complexity: O(n)
+Space Complexity: O(min(m,n)) where m is the size of the character set */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int l = 0, r = 0, len = 0;
+        unordered_set <int> hs;
+
+        while(r < s.length())
+        {
+            if(hs.find(s[r]) == hs.end()){
+                hs.insert(s[r]);
+                r++;
+                len = max(len, r - l);        // window length/ hashset length = r - l (after r increment ofc)
+            }
+            else{
+                hs.erase(s[l]);
+                l++;
+            }
+        }
+        return len;
+    }
+};
+
+int main()
+{
+    Solution s;
+    string str = "abcabcbb";
+    cout << s.lengthOfLongestSubstring(str) << endl; // 3
+    return 0;
+}
