a79fab6ce540387e80cd5f621b9dd2c1c0150fad
asef-mahir <darkknight5812@gmail.com>
2026-05-17 01:23:26 +0600

    solved a hard 154 and added comments to 153 also

--- Algorithms/Binary_Search/LC_154_find_minimum_rotated_sortedArray_II.cpp
+++ Algorithms/Binary_Search/LC_154_find_minimum_rotated_sortedArray_II.cpp
@@ -1,7 +1,7 @@
-/*Problem: Find Minimum in Rotated Sorted Array
-Problem link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/
-Technique: Binary Search with some modifications (Pivot finding)
-*Constraints: Problem has to be solved in O(log n) time complexity, so we cannot do linear search, we have to use binary search
+/*Problem: Find Minimum in Rotated Sorted Array II
+Problem link: https://leetcode.com/problems/find-minimum-in-rotated-sorted-array-ii/ 
+Technique: Binary Search with some modifications (Pivot finding) & handling duplicates
+*Constraints: Problem has to be solved in O(log n) time complexity instead of O(n)
 **Time Complexity: O(log n) , Space Complexity: O(1)
 */
 
@@ -9,18 +9,28 @@ Technique: Binary Search with some modifications (Pivot finding)
 using namespace std;
 
 class Solution {
-public:
-    int findMin(vector<int> &nums) {
+public: 
+    int findMin(vector<int>& nums) {
         int l = 0, r = nums.size() - 1, mid, res = INT_MAX;
         while(l <= r)
         {
+            // Skip duplicates from the left
+            while(l < r && nums[l] == nums[l+1]){
+                l++;
+            }
+            // Skip duplicates from the right
+            while(l < r && nums[r] == nums[r-1]){
+                r--;
+            }
+
             // If array is already sorted, then the minimum element is the leftmost element
             if(nums[r] > nums[l]){
                 res = min(res, nums[l]);
-                break;
             }
+
             mid = l + (r - l) / 2;
             res = min(res, nums[mid]);
+
             //If this condition is true, it means the left half is sorted, so the minimum element must be in the right half
             if(nums[mid] >= nums[l]){
                 l = mid + 1;
@@ -33,10 +43,9 @@ public:
     }
 };
 
-int main()
-{
-    Solution s;
-    vector<int> nums = {3,4,5,1,2};
-    cout << s.findMin(nums) << endl;
+int main(){
+    Solution sol;
+    vector<int> nums = {2, 2, 2, 0, 1};
+    cout << sol.findMin(nums) << endl; // Output: 0
     return 0;
 }
